d14927e550d4ca2f37b74a9fc4e00fa0c661ac65
Dawn-used-yeet <prateekvarshney04032008@gmail.com>
2025-02-16 14:35:38 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,6 +9,7 @@ dependencies {
     implementation(project(":ext"))
     val libVersion: String by project
     compileOnly("com.github.brahmkshatriya:echo:$libVersion")
+    implementation("com.neovisionaries:nv-websocket-client:2.14")
 }
 
 val extType: String by project
